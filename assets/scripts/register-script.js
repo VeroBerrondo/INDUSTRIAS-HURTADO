e33f8d3dcee865584ad82ccb5faed49137edd34d
VeroBerrondo <veronick78.vb@gmail.com>
2024-04-09 12:32:15 -0300

    ..

--- assets/scripts/register-script.js
+++ assets/scripts/register-script.js
@@ -1,4 +1,4 @@
-function nextPanel () {
+function nextPanel() {
     var appWrapper_One = document.getElementsByClassName("formulario-caja")[0];
     var appWrapper_Two = document.getElementsByClassName("formulario-caja2")[0];
 
@@ -6,4 +6,10 @@ function nextPanel () {
     TwoStatus = appWrapper_Two.classList.contains("visible");*/
     appWrapper_One.classList.remove("visible");
     appWrapper_Two.classList.add("visible");
+}
+function TogglePassword() {
+    const passwordField = document.getElementById("password");
+    if (passwordField.type === "password") { passwordField.type = "text" } else {
+        passwordField.type = "password";
+    }
 }
