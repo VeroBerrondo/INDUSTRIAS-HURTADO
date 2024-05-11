64ed8a80b074ce9dd8d816c135ac52198584379b
VeroBerrondo <veronick78.vb@gmail.com>
2024-05-11 02:03:03 -0300

    .

--- assets/scripts/registroFireBase.js
+++ assets/scripts/registroFireBase.js
@@ -90,7 +90,7 @@ window.savePreferences = async function savePreferences() {
     });
 
     alert('Preferencias guardadas correctamente.');
-    window.location.href = "inicioDeSesion.html";
+    window.location.href = "login.html";
   } catch (error) {
     console.error('Error al guardar preferencias:', error);
     alert('Error al guardar preferencias: ' + error.message);
