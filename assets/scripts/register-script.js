6dc0c2427396e4ca1012a5dba5f4438eff3b340f
VeroBerrondo <veronick78.vb@gmail.com>
2024-05-26 00:19:32 -0300

    .

--- assets/scripts/register-script.js
+++ assets/scripts/register-script.js
@@ -1,3 +1,19 @@
+
+document.getElementById('btn').addEventListener('click', function () {
+    let name = document.getElementById('name').value;
+    let lastname = document.getElementById('lastname').value;
+    let nameUser = document.getElementById('nameUser').value;
+    let birthdate = document.getElementById('birthdate').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    if (!name || !lastname || !nameUser || !birthdate || !email || !password) {
+        alert('Por favor, complete todos los campos.');
+    } else {
+        document.getElementById('registrationForm').style.display = 'none';
+        document.getElementById('preferencesForm').style.display = 'block';
+    }
+});
 function nextPanel() {
     var appWrapper_One = document.getElementsByClassName("formulario-caja")[0];
     var appWrapper_Two = document.getElementsByClassName("formulario-caja2")[0];
